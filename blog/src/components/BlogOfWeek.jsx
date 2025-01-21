8523aa9df638cea7ec218cd512ec2133e4dc5d4c
softwareNinja-888 <mlandvodlamini03@gmail.com>
2025-01-21 09:21:50 +0200

    Add display pages for blogs

--- blog/src/components/BlogOfWeek.jsx
+++ blog/src/components/BlogOfWeek.jsx
@@ -1,7 +1,7 @@
 export function BlogOfWeek(){
     return (
         <>
-            <div className="flex bg-blogBackground bg-center bg-cover bg-no-repeat h-72 px-5 py-5 relative w-10/12">
+            <div className="flex bg-blogBackground bg-center bg-cover bg-no-repeat h-72 px-5 py-5 relative w-10/12 border border-black dark:border-white">
                 <div className="absolute inset-0 bg-black bg-opacity-30 pointer-events-none"></div>
                 <div className="flex flex-col gap-4 relative z-10 justify-center">
                     <div className="text-white font-poppins text-lg">Blog of the Week</div>
